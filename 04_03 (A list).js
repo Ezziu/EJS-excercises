5e926362ded334a42f43b86c341616fe436dbbc0
Ezziu <34916180+Ezziu@users.noreply.github.com>
2018-01-08 14:47:11 +0100

    Update 04_03 (A list).js

--- 04_03 (A list).js
+++ 04_03 (A list).js
@@ -1,13 +1,23 @@
-
 function arrayToList(array){
-  var listrest = null;
-  for (var i = array.length-1; i >= 0; i--){
-    //list.value = array[i-1];
-  listrest = {value: array[i], rest: listrest}; 
-    //listrest = list.rest;
+   var list = null;
+   for (i = array.length-1; i >= 0; i--){
+     list = {value: array[i], rest: list}; 
   }
-   return listrest;
+  return list;
 }
+
+console.log(typeof list);
+
+function listToArray(list){
+  var arr = [];
+  for (var node = list; node; node = node.rest){ 
+  	arr.push(node.value);
+  }
+  return arr;
+}  
 
 console.log(arrayToList([10, 20, 30]));
 // → {value: 10, rest: {value: 20, rest: null}}
+
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
