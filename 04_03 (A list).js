4c719763b0fe03cd1c34c0effd5c76895b975aa3
Ezziu <34916180+Ezziu@users.noreply.github.com>
2018-01-08 15:38:18 +0100

    Update 04_03 (A list).js

--- 04_03 (A list).js
+++ 04_03 (A list).js
@@ -16,8 +16,27 @@ function listToArray(list){
   return arr;
 }  
 
+function prepend(valueE, list){
+  listN = {value: valueE, rest: list};
+  return listN;
+}
+
+function nth(list, pos){
+ var arr = [];
+  for (var node = list; node; node = node.rest){ 
+  	arr.push(node.value);
+	list = arr[pos];  
+  }
+  return list;
+}
+
 console.log(arrayToList([10, 20, 30]));
 // → {value: 10, rest: {value: 20, rest: null}}
-
 console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
+console.log(prepend(100, prepend(200, null)));
+// → {value: 100, rest: {value: 200, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 2));
+// → 30
+
+
