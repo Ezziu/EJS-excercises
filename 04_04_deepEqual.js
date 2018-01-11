80a3ca147a53ce7f189ec6d2ff46c9ae0846f4bf
Ezziu <34916180+Ezziu@users.noreply.github.com>
2018-01-11 22:22:22 +0100

    Create 04_04_deepEqual.js

---
+++ 04_04_deepEqual.js
@@ -0,0 +1,39 @@
+function deepEqual(val1, val2){
+  if (val1 == val2)
+    return true;
+  
+//  else if ((typeof val1 !== "object" || val1 == null) || (typeof val2 !== "object" || val2 == null))
+ //   return false;
+   
+  else if ((typeof val1 == "object" && val1 !== null) && (typeof val2 == "object" || val2 !== null)){
+    
+      var xInVal1=0;
+      var xInVal2=0;
+    
+      for (x in val1) {
+        xInVal1 += 1;
+       } 
+      for (x in val2){
+        xInVal2 += 1; 
+        console.log(xInVal2);
+          if (deepEqual(val1[x], val2[x]) !== true)
+            return false;  
+       
+      }
+    if (xInVal1 == xInVal2)
+      return true;
+      
+  }
+  else return false;
+}  
+
+
+
+var obj = {here: {is: "an"}, object: 2};
+
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
